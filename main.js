3461d0b4a29c34827a071daac18ba3994568d039
wuaan0903 <110773076+wuaan0903@users.noreply.github.com>
2022-08-21 17:15:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector(".header")
     header.classList.toggle("sticky",window.scrollY>0)
 })
+
+const toggle = document.querySelector(".menu-right-mobile"),
+menu = document.querySelector(".menu-mobile")
+
+toggle.addEventListener("click",()=>{
+    menu.classList.toggle("open")
+    toggle.classList.toggle("toggle")
+})
