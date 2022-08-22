61f8b5bde0e52138aa8e35896a6b1138ef0d49ce
wuaan0903 <110773076+wuaan0903@users.noreply.github.com>
2022-08-22 10:41:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,28 @@ window.addEventListener("scroll",function(){
 })
 
 const toggle = document.querySelector(".menu-right-mobile"),
-menu = document.querySelector(".menu-mobile")
+menu = document.querySelector(".menu-mobile"),
+iconToggle = document.querySelector(".menu1"),
+iconClose = document.querySelector(".close")
 
 toggle.addEventListener("click",()=>{
     menu.classList.toggle("open")
     toggle.classList.toggle("toggle")
+})
+
+
+$(document).ready(function()
+{
+    $(".grid-col>.row").click(function(){
+        $(this).children(".content-footer").slideToggle();
+        $(this).children(".title-footer1").children("i").toggleClass('bx-rotate-270')
+    })
+})
+
+$(document).ready(function()
+{
+    $(".last-row").click(function(){
+        $(this).children(".content-footer").slideToggle();
+        $(this).children(".title-footer1").children("i").toggleClass('bx-rotate-270')
+    })
 })
